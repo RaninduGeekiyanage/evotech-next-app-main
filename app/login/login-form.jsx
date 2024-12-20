0155dbb17eaa668a3093a69ea6740e3dabb02525
Ranindu Geekiyanage <ranindu.rag@gmail.com>
2024-12-20 20:47:18 +0530

    add shadcn

--- app/login/login-form.jsx
+++ app/login/login-form.jsx
@@ -1,14 +1,53 @@
 'use client'
 
 // use for client side (brwser side) CSR
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginForm() {
-  return (
-    <div>
-        <div className="w-[400px] mx-auto">
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailError, setEmailError] = useState("")
+    const [passwordError, setPasswordError] = useState("");
+
+
+    const validateForm = () => {
+        if (!email) {
+            setEmailError("Email is required..!")
+            return false
+        } else {
+            setEmailError("");
+        }
+
+
+        if (!password) {
+            setPasswordError("Passowrd is required..!")
+            return false
+        } else {
+            setPasswordError("")
+        }
+
+        return true
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const isValid = validateForm();
+
+        if (isValid) {
+            //login form data submition
+            console.log("Form Data :", { email: email, password: password });
+        }
+    }
+
+
+    return (
+        <div>
+            <div className="w-[400px] mx-auto">
                 <div className="bg-white shadow-md border border-gray-200 rounded-lg p-4">
                     <form onSubmit={handleSubmit} className="space-y-6">
+                        {/* title */}
                         <h3 className="text-center text-xl font-semibold text-gray-900">
                             Sign in to Evotech
                         </h3>
@@ -25,9 +64,13 @@ export default function LoginForm() {
                                 type="email"
                                 name="email"
                                 id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 placeholder="your email"
                                 className="bg-gray-50 border text-gray-900 rounded-lg  focus:ring-blue-500 focus:border-blue-600 block w-full p-2.5"
                             />
+                            {/* email error message */}
+                            {emailError && <div className='text-red-600 text-sm mt-2 ml-1'>{emailError}</div>}
                         </div>
 
                         {/* password div*/}
@@ -42,13 +85,17 @@ export default function LoginForm() {
                                 type="password"
                                 name="password"
                                 id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 placeholder="password"
                                 className="bg-gray-50 border text-gray-900 rounded-lg focus:ring-blue-500 focus: focus:border-blue-600 block w-full p-2.5"
                             />
+                            {/* password error message */}
+                            {passwordError && <div className='text-red-600 text-xs mt-2 ml-1'>{passwordError}</div>}
                         </div>
 
 
-                         {/* checkbox area */}
+                        {/* checkbox area */}
                         <div className="flex justify-between">
                             <div className="flex items-start">
                                 <div className="flex items-center h-5">
@@ -92,6 +139,6 @@ export default function LoginForm() {
                     </form>
                 </div>
             </div>
-    </div>
-  )
+        </div>
+    )
 }
